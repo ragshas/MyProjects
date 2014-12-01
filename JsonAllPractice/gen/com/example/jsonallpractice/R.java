0be08c066b64068e19e71eb93a0827c701596b2c
Raghab Shrestha <ragshas@gmail.com>
2014-12-01 17:16:03 +0545

    After completion of class

--- JsonAllPractice/gen/com/example/jsonallpractice/R.java
+++ JsonAllPractice/gen/com/example/jsonallpractice/R.java
@@ -1094,7 +1094,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050045;
+        public static final int action_settings=0x7f050048;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
@@ -1105,7 +1105,8 @@ containing a value of this type.
         public static final int disableHome=0x7f050008;
         public static final int dropdown=0x7f05000f;
         public static final int edit_query=0x7f050030;
-        public static final int email=0x7f050043;
+        public static final int email=0x7f050046;
+        public static final int email_label=0x7f050042;
         public static final int end=0x7f050013;
         public static final int expand_activities_button=0x7f050026;
         public static final int expanded_menu=0x7f05002c;
@@ -1119,13 +1120,15 @@ containing a value of this type.
         public static final int list_item=0x7f050029;
         public static final int listview=0x7f05003f;
         public static final int middle=0x7f050012;
-        public static final int mobile=0x7f050044;
-        public static final int name=0x7f050042;
+        public static final int mobile=0x7f050047;
+        public static final int mobile_label=0x7f050043;
+        public static final int name=0x7f050045;
+        public static final int name_label=0x7f050041;
         public static final int name_textView=0x7f05003d;
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
-        public static final int product_label=0x7f050041;
+        public static final int product_label=0x7f050044;
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
@@ -1208,10 +1211,11 @@ containing a value of this type.
         public static final int activity_json_example1=0x7f030019;
         public static final int activity_json_example__multiple_list_view=0x7f03001a;
         public static final int activity_list_view_activity1=0x7f03001b;
-        public static final int activity_single_list_item=0x7f03001c;
-        public static final int contact_list_item=0x7f03001d;
-        public static final int list_item=0x7f03001e;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001f;
+        public static final int activity_single_contact=0x7f03001c;
+        public static final int activity_single_list_item=0x7f03001d;
+        public static final int contact_list_item=0x7f03001e;
+        public static final int list_item=0x7f03001f;
+        public static final int support_simple_spinner_dropdown_item=0x7f030020;
     }
     public static final class menu {
         public static final int json_contacts=0x7f0d0000;
@@ -1219,7 +1223,8 @@ containing a value of this type.
         public static final int json_example__multiple_list_view=0x7f0d0002;
         public static final int list_view_activity1=0x7f0d0003;
         public static final int main=0x7f0d0004;
-        public static final int single_list_item=0x7f0d0005;
+        public static final int single_contact=0x7f0d0005;
+        public static final int single_list_item=0x7f0d0006;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1271,6 +1276,7 @@ containing a value of this type.
         public static final int title_activity_json_example1=0x7f0a000f;
         public static final int title_activity_json_example__multiple_list_view=0x7f0a0014;
         public static final int title_activity_list_view_activity1=0x7f0a0016;
+        public static final int title_activity_single_contact=0x7f0a0019;
         public static final int title_activity_single_list_item=0x7f0a0017;
         public static final int url=0x7f0a0013;
         public static final int wid=0x7f0a0011;
